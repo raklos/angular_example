a517784e22184820ce0281b9de3760b35d60b0d7
Rafał Kłosiński <klosinski.rafal@o2.pl>
2022-08-24 21:01:36 +0200

    modified:   src/app/app.component.css 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/box/box.component.css 	new file:   src/app/box/box.component.html 	new file:   src/app/box/box.component.spec.ts 	new file:   src/app/box/box.component.ts 	new file:   src/app/header/header.component.css 	new file:   src/app/header/header.component.html 	new file:   src/app/header/header.component.spec.ts 	new file:   src/app/header/header.component.ts 	new file:   src/app/tools/tools.component.css 	new file:   src/app/tools/tools.component.html 	new file:   src/app/tools/tools.component.spec.ts 	new file:   src/app/tools/tools.component.ts 	modified:   src/styles.css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ToolsComponent } from './tools/tools.component';
+import { BoxComponent } from './box/box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ToolsComponent,
+    BoxComponent
   ],
   imports: [
     BrowserModule,
